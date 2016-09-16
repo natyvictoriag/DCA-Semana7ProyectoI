03731b6440447d2f7bd50bf5b9f4f76f5137c271
Usuario <Usuario@172.30.112.232>
2016-09-16 15:29:16 -0500

    MainApp

--- DCAProyectoIntegrador/src/MainApp.java
+++ DCAProyectoIntegrador/src/MainApp.java
@@ -1,4 +1,23 @@
+import processing.core.PApplet;
 
-public class MainApp {
-//guachu
+public class MainApp extends PApplet {
+	public static void main(String[] args) {
+		PApplet.main("MainApp");
+	}
+
+	public void settings() {
+     size(1000,800);
+	}
+
+	public void setup() {
+     
+	}
+
+	public void draw() {
+     background(255);
+	}
+
+	public void mousePressed() {
+
+	}
 }

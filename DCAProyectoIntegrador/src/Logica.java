3f1dcbf7523c682aaa35140fdd0baf9726eb64c7
USUARIO <USUARIO@MSI.academico.icesi.edu.co>
2016-09-27 19:25:30 -0500

    se establecieron los files

--- DCAProyectoIntegrador/src/Logica.java
+++ DCAProyectoIntegrador/src/Logica.java
@@ -6,49 +6,54 @@ import java.awt.Image;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Logica {
-	
+
 	PApplet app;
-	
+
 	private Logica log;
-	private int play = 0, posX = 0, posY = 0, numIma = 4, rsize = 0,rota = 0, y = 87, var = 0;
-	private boolean aumento = true, gira = false, completa =false;
+	private int play = 0, posX = 0, posY = 0, numIma = 4, rsize = 0, rota = 0, y = 87, var = 0;
+	private boolean aumento = true, gira = false, completa = false;
 	private PImage[] fondo;
-	private String[] clases = {".png", ".jpg", ".bmp"};
+	private String[] clases = { ".png", ".jpg", ".bmp" };
 	private PImage[] ima = new PImage[20];
 	private PImage[] imaPrinsipal = new PImage[20];
-	// private PImage nort;
-	// arreglo de imagenes
-	
-	//======================================================================================================================
-	
-	public Logica (PApplet app) {
-		
-		
-		
-//		imagenes = new ArrayList<Image>();
-//		file = new File("data/images");
-//		list = file.list();
-//		folder= file.getPath();
-//		images = new PImage[list.length];
-//		
-//		for (int i = 0; i < list.length; i++) {
-//			System.out.println(list[i]);
-//			
-//			images[i]= this.app.loadImage(folder + list[i]);
-//		}
+
+	// ======================================================================================================================
+
+	public Logica(PApplet app) {
+
+		this.app = app;
+
+		// file que conecta a la carpeta
+		File carpeta = new File("../data/images");
+
+		// arreglo con los tipos de archivos
+		String[] clases = new String[] { "jpg", "png", "bmp" };
+
+		// identifica las extensiones de las imagenes
+
+		FilenameFilter filtroIma = new FilenameFilter() {
+
+			public boolean accept(File carpeta, String nombre) {
+				for (final String ext : clases) {
+					if (nombre.endsWith("." + ext)) {
+						return true;
+					}
+				}
+				return false;
+			}
+		};
 
 	}
-	
-	//======================================================================================================================
-	
+
+	// ======================================================================================================================
+
 	public void pintar() {
 
 	}
-	
-	//======================================================================================================================
-	
+
+	// ======================================================================================================================
+
 	public void click() {
 
 	}
